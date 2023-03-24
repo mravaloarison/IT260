8114d64118d55f370301ec20ce1cce9e15de11ec
mami-em <maminiainaravaloarison@gmail.com>
2023-03-24 17:00:45 -0400

    design

--- project3/app.cs
+++ project3/app.cs
@@ -1,18 +1,24 @@
 using System;
 
 class Program {
-    static bool IsValid(string cardNumber) {
+    static bool IsValid(string cardNumber) 
+    {
         int sum = 0;
         bool isSecondDigit = false;
-        for (int i = cardNumber.Length - 1; i >= 0; i--) {
-            if (cardNumber[i] < '0' || cardNumber[i] > '9') {
+        for (int i = cardNumber.Length - 1; i >= 0; i--) 
+        {
+            if (cardNumber[i] < '0' || cardNumber[i] > '9') 
+            {
                 // Invalid character, return false
                 return false;
             }
+            
             int digit = cardNumber[i] - '0';
-            if (isSecondDigit) {
+            if (isSecondDigit) 
+            {
                 digit *= 2;
-                if (digit > 9) {
+                if (digit > 9) 
+                {
                     digit -= 9;
                 }
             }
@@ -22,25 +28,42 @@ class Program {
         return sum % 10 == 0;
     }
     
-    static string GetCardType(string cardNumber) {
-        if (cardNumber.Length == 15 && (cardNumber.StartsWith("34") || cardNumber.StartsWith("37"))) {
+    static string GetCardType(string cardNumber) 
+    {
+        if (cardNumber.Length == 15 && (cardNumber.StartsWith("34") || cardNumber.StartsWith("37"))) 
+        {
             return "American Express";
-        } else if (cardNumber.Length == 16 && (cardNumber.StartsWith("51") || cardNumber.StartsWith("52") || cardNumber.StartsWith("53") || cardNumber.StartsWith("54") || cardNumber.StartsWith("55"))) {
+        } 
+        
+        else if (cardNumber.Length == 16 && (cardNumber.StartsWith("51") || cardNumber.StartsWith("52") || cardNumber.StartsWith("53") || cardNumber.StartsWith("54") || cardNumber.StartsWith("55"))) 
+        {
             return "MasterCard";
-        } else if ((cardNumber.Length == 13 || cardNumber.Length == 16) && cardNumber.StartsWith("4")) {
+        } 
+        
+        else if ((cardNumber.Length == 13 || cardNumber.Length == 16) && cardNumber.StartsWith("4")) 
+        {
             return "Visa";
-        } else {
+        } 
+        
+        else 
+        {
             return "Unknown";
         }
     }
 
-    static void Main() {
+    static void Main() 
+    {
         Console.Write("Enter credit card number: ");
         string cardNumber = Console.ReadLine();
-        if (IsValid(cardNumber)) {
+
+        if (IsValid(cardNumber)) 
+        {
             Console.WriteLine("Valid credit card number!");
             Console.WriteLine($"Card type: {GetCardType(cardNumber)}");
-        } else {
+        } 
+        
+        else 
+        {
             Console.WriteLine("Invalid credit card number!");
         }
     }
